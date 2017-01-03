5b5f84d6d250afb31775875695e7c07bd92dd358
Ricardo M. Ferraz Leal <ricleal@gmail.com>
2017-01-03 11:04:04 -0500

    added get_all_ipts_for_an_uid

--- server/apps/users/ldap_util.py
+++ server/apps/users/ldap_util.py
@@ -29,6 +29,8 @@ class LdapSns(object):
 
     def get_all_ipts(self):
         '''
+        @return list of IPTSs: ['IPTS-18360', 'IPTS-18361', 'IPTS-17692']
+ 
         >>> all_ipts[-1]
         ('cn=IPTS-17757,ou=Groups,dc=sns,dc=ornl,dc=gov', {'cn': [b'IPTS-17757']})
         '''
@@ -43,6 +45,7 @@ class LdapSns(object):
 
     def get_all_uids_for_an_ipts(self,ipts):
         '''
+        @return list of uids: ['djk', 'jameshutchison', 'sandercr', 'hbd', 'q3n']
         '''
         results = self.server.search_s(
             "ou=Groups,dc=sns,dc=ornl,dc=gov",
@@ -58,6 +61,16 @@ class LdapSns(object):
 
     def get_all_users_and_ipts(self):
         '''
+        @return: lists of:   {'cn': [b'IPTS-17329'],
+                               'memberUid': [b'ckt',
+                                             b'boehler',
+                                             b'biancahaberl',
+                                             b'l8d',
+                                             b'yyc',
+                                             b'ri2',
+                                             b'j7t']})
+        Not used!!
+        
         >>> all_users_and_ipts[-1]
         ('cn=IPTS-17329,ou=Groups,dc=sns,dc=ornl,dc=gov', {'cn': [b'IPTS-17329'], 'memberUid': [b'ckt', b'boehler', b'biancahaberl', b'l8d', b'yyc', b'ri2', b'j7t']})
         >>> all_users_and_ipts[-1][1]
@@ -73,19 +86,50 @@ class LdapSns(object):
         return results
 
     def get_all_users_name_and_uid(self):
+        '''
+        @return: list of {'name': 'Whitaker, Tracy H', 'uid': 'twq'},
+                            {'name': 'Williamson, Richard L', 'uid': 'wrt'}]
+        '''
         results = self.server.search_s(
             "ou=Users,dc=sns,dc=ornl,dc=gov",
             ldap.SCOPE_SUBTREE,
             "(objectClass=posixAccount)",
             ["cn","uid"]
         )
-        return [ { 'uid' : result[1]['uid'][0].decode('UTF-8'), 'name' : result[1]['cn'][0].decode('UTF-8') } for result in results]
+        return [{'uid':result[1]['uid'][0].decode('UTF-8'), 'name' : result[1]['cn'][0].decode('UTF-8') } for result in results]
+
+    def get_all_ipts_for_an_uid(self, uid):
+        '''
+        return a list of IPTSs: [ 'IPTS-7007', 'IPTS-6292']
+        '''
+        results = self.server.search_s(
+            "ou=Groups,dc=sns,dc=ornl,dc=gov",
+            ldap.SCOPE_SUBTREE,
+            "(&(cn=IPTS*)(description=proposal)(memberUid=%s))"%uid,
+            ["cn"]
+        )
+        #return results
+        return [result[1]['cn'][0].decode('UTF-8') for result in results]
+        
 
 
-
-
-# l = setup()
-# all_ipts = get_all_ipts(l)
-# uids = get_all_uids_for_an_ipts(l,all_ipts[-1])
-# users =  get_all_users_name_and_uid(l)
-# l.unbind()
+# Test
+if __name__ == '__main__':
+    from pprint import pprint
+    l = LdapSns()
+    
+    #all_ipts = l.get_all_ipts()
+    #pprint(all_ipts)
+    
+    #all_uids_for_an_ipts = l.get_all_uids_for_an_ipts(all_ipts[-1])
+    #pprint(all_uids_for_an_ipts)
+    
+    #all_users_and_ipts = l.get_all_users_and_ipts()
+    #pprint(all_users_and_ipts)
+    
+    #all_users_name_and_uid = l.get_all_users_name_and_uid()
+    #pprint(all_users_name_and_uid)
+    
+    all_ipts_for_an_uid = l.get_all_ipts_for_an_uid("19g")
+    pprint(all_ipts_for_an_uid)
+    
