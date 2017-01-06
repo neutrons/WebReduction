49bca51b7f346de93d00f1d08b3fd348ff953237
Ricardo M. Ferraz Leal <ricleal@gmail.com>
2017-01-06 15:09:06 -0500

    Fab file working

---
+++ config/certificates/README.com
@@ -0,0 +1,7 @@
+# Fake certificates for testing
+
+```
+openssl req \
+       -newkey rsa:2048 -nodes -keyout domain.key \
+       -x509 -days 365 -out domain.crt
+```
--- fabfile.py
+++ fabfile.py
@@ -3,69 +3,150 @@ This file is never called direcly
 Use the command `fab`
 
 If running this for the first time or without vietualenv:
-pip3 install Fabric3 python-dotenv --user
+pip3 install Fabric3  --user
+
+fab -f fabfile_back2.py -R dev deploy_dev
 
 '''
+from __future__ import print_function, with_statement
 
-from fabric.api import task, run, env, local, path, prefix, cd
+from fabric.api import task, run, env, local, path, prefix, cd, hosts, roles, parallel, settings, sudo
+from fabric.contrib import files
 import os
+import time
 
-@task
-def dev():
-    env.user = 'rhf'
-    env.hosts = ['lealpc.ornl.gov']
+# Fabric hangs without this
+env.shell = "/bin/bash -c"
 
-@task
-def staging():
-    env.user = 'rhf'
-    env.hosts = ['pc83734.ornl.gov']
+env.roledefs = {
+    'dev': {
+        'hosts': ['localhost'],
+        'user': 'rhf',
+        'project_root': '/home/rhf/git/sns-reduction',
+    },
+    'dev_ssl': {
+        'hosts': ['localhost'],
+        'user': 'rhf',
+        'project_root': '/home/rhf/git/sns-reduction',
+    },
+    'staging': {
+        'hosts': ['pc83734.ornl.gov'],
+        'user': 'rhf',
+        'project_root': '/home/rhf/git/sns-reduction',
+    },
+    'production': {
+        'hosts': ['reduction.sns.gov'],
+        'user': 'rhf',
+        'project_root': '/var/www/sns-reduction',
+    }
+}
 
-@task
-def prod():
-    env.user = 'rhf'
-    env.hosts = ['reduction.sns.gov']
+# Dev
+env.roledefs['dev'].update({'nginx_conf_template' : os.path.join(env.roledefs['dev']['project_root'], 'config', 'deploy', 'nginx_dev_template.conf' ) })
+env.roledefs['dev'].update({'nginx_conf_file' : os.path.join(env.roledefs['dev']['project_root'], 'dist', 'nginx.conf' ) })
+env.roledefs['dev'].update({'uwsgi_ini_template' : os.path.join(env.roledefs['dev']['project_root'], 'config', 'deploy', 'uwsgi_template.ini' ) })
+env.roledefs['dev'].update({'uwsgi_ini_file' : os.path.join(env.roledefs['dev']['project_root'], 'dist', 'uwsgi.ini' ) })
+env.roledefs['dev'].update({'requirements_file' : os.path.join(env.roledefs['dev']['project_root'], 'requirements.txt' ) })
+# Dev_SSL
+env.roledefs['dev_ssl'].update(env.roledefs['dev'])
+env.roledefs['dev_ssl'].update({'nginx_conf_template' : os.path.join(env.roledefs['dev']['project_root'], 'config', 'deploy', 'nginx_staging_template.conf' ) })
 
-# settings.py
+# Staging
+env.roledefs['staging'].update(env.roledefs['dev_ssl'])
 
-PROJECT_ROOT = os.path.dirname(os.path.realpath(__file__))
-DIST_ROOT = os.path.join(PROJECT_ROOT, 'dist')
-VENV_ROOT =  os.path.join(PROJECT_ROOT, 'venv')
-SRC_ROOT =  os.path.join(PROJECT_ROOT, 'src')
+# Production
+env.roledefs['production'].update(env.roledefs['staging'])
+env.roledefs['production'].update({'ssl_certificate_file' : '/etc/ssl/certs/wildcard.sns.gov.crt'})
+env.roledefs['production'].update({'ssl_certificate_key_file' : '/etc/pki/tls/private/wildcard.sns.gov.key'})
 
-
-def prepare_deploy(branch='master'):
+#
+# Aux functions
+#
+def run_background(command, run_as_sudo=False):
     '''
-    Run for example as: fab prepare_deploy:de
+    Rus remote command in the background
     '''
-    assert(branch in ['master','dev'])
-    local("git checkout %s"%branch)
-    local("git pull origin %s"%branch)
+    full_command = "set -m; nohup %s >& /dev/null < /dev/null &"%command
+    if run_as_sudo:
+        sudo(full_command, pty=False)
+    else:
+        run(full_command, pty=False)
 
-@task
+#
+# Main Functions
+#
+
+def restart_nginx(run_as_sudo=False):
+    # Start nginx
+    with settings(warn_only=True):
+        command = "killall -w -q -s INT $(which nginx)"
+        if run_as_sudo:
+            sudo(command)
+        else:
+            run(command)
+    run_background("nginx -c %(nginx_conf_file)s -g 'daemon off;'"%env, run_as_sudo)
+
+def update_env():
+    context = env.roledefs[env.effective_roles[0]]
+    env.update(context)
+    from pprint import pprint
+    pprint(env)
+
+
 def deploy():
-    # Create a directory on a remote server, if it doesn't already exists
-    if not os.path.isdir(DIST_ROOT):
-        run('mkdir -p %s'%DIST_ROOT)
+    venv_root = os.path.join(env['project_root'], 'venv')
+    src_root = os.path.join(env['project_root'], 'src')
 
-    # Create a virtualenv, if it doesn't already exists
-    if not os.path.isdir(VENV_ROOT):
-        with cd(PROJECT_ROOT):
+    # Create Virtual env
+    if not os.path.isdir(venv_root):
+        with cd(env['project_root']):
             run('virtualenv venv')
 
     # Activate the environment and install requirements
-    with prefix('. ' + VENV_ROOT + '/bin/activate'):
-        run('pip install -U -r requirements.txt')
+    with settings(warn_only=True), prefix('. ' + venv_root + '/bin/activate'):
+        run("pip install -U -r %(requirements_file)s"%env)
 
-    with cd(SRC_ROOT), prefix('. ' + VENV_ROOT + '/bin/activate'):
+    with cd(src_root), prefix('. ' + venv_root + '/bin/activate'):
         # Collect all the static files
         run('python manage.py collectstatic --noinput')
-
         # Migrate and Update the database
         run('python manage.py makemigrations --noinput')
         run('python manage.py migrate --no-input')
         run('python manage.py migrate --no-input')
         run('python manage.py loaddata catalog')
 
-    
-    # uwsgi --ini config/deploy/uwsgi_dev.ini
-    # /usr/sbin/nginx -c $(pwd)/config/deploy/nginx_dev.conf -g 'daemon off;'
+    # Fills in the templates
+    files.upload_template(env['nginx_conf_template'], env['nginx_conf_file'], context=env)
+    files.upload_template(env['uwsgi_ini_template'], env['uwsgi_ini_file'], context=env)
+
+    with prefix('. ' + venv_root + '/bin/activate'), settings(warn_only=True):
+        run("killall -w -q -s INT $(which uwsgi)")
+        run_background("uwsgi --ini %(uwsgi_ini_file)s"%env)
+
+
+#
+# Tasks
+#
+@task
+@roles('dev')
+def deploy_dev():
+    update_env()
+    deploy()
+    restart_nginx()
+
+@task
+@roles('dev_ssl')
+def deploy_dev_ssl():
+    update_env()
+    deploy()
+    restart_nginx(run_as_sudo=True)
+
+@task
+@roles('staging')
+def deploy_staging():
+    pass
+
+@task
+@roles('production')
+def deploy_prod():
+    pass
