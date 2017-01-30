fca7b8ae85ace3371381a57bab143cff6eb1d944
Ricardo M. Ferraz Leal <ricleal@gmail.com>
2017-01-30 17:53:27 -0500

    Tests with thumbnails in base64

--- src/server/apps/catalog/hfir/util.py
+++ src/server/apps/catalog/hfir/util.py
@@ -11,12 +11,13 @@ import logging
 logger = logging.getLogger('catalog.hfir')
 
 class Parser(object):
-    
+    '''
+    Generic HFIR files parser
+    '''
     def __init__(self, filename):
         if not os.path.exists(filename):
             logger.error("File {} does not exist!".format(filename))
             sys.exit()
-        
         self._root = self._parse(filename)
     
     def _parse(self, filename):
@@ -57,11 +58,16 @@ class Parser(object):
 
 
 if __name__ == "__main__":
-    
+    '''
+    Stupid test:
+    Reads a Biosans folder and generates images for both main and wing detector
+    Images are in base64
+    Both linear and log
+    '''
     from PIL import Image
     from io import BytesIO
     from base64 import b64encode
-    from matplotlib.colors import Normalize
+    from matplotlib.colors import Normalize, LogNorm
     from matplotlib import cm
     import glob
 
@@ -72,6 +78,8 @@ if __name__ == "__main__":
             p = Parser(input_file)
             data_main_detector = p.getData("Data/Detector")
             data_wing_detector = p.getData("Data/DetectorWing")
+
+            f.write("<pre>Main / Wing / Log(Main) / Log(wing) :: {}</pre>".format(input_file)) 
 
             b = BytesIO()
             norm = Normalize(clip=True)
@@ -84,6 +92,24 @@ if __name__ == "__main__":
 
             b = BytesIO()
             norm = Normalize(clip=True)
+            c = cm.viridis(norm(data_wing_detector), bytes=True)
+            Image.fromarray(c).save(b, format='png')
+            data_as_binary = b64encode(b.getvalue())
+            data_as_string = data_as_binary.decode('utf-8')
+            s=('<img src="data:image/png;base64,{!s}">'.format(data_as_string))
+            f.write(s+"&nbsp;") 
+
+            b = BytesIO()
+            norm = LogNorm(clip=True)
+            c = cm.viridis(norm(data_main_detector), bytes=True)
+            Image.fromarray(c).save(b, format='png')
+            data_as_binary = b64encode(b.getvalue())
+            data_as_string = data_as_binary.decode('utf-8')
+            s=('<img src="data:image/png;base64,{!s}">'.format(data_as_string))
+            f.write(s+"&nbsp;") 
+
+            b = BytesIO()
+            norm = LogNorm(clip=True)
             c = cm.viridis(norm(data_wing_detector), bytes=True)
             Image.fromarray(c).save(b, format='png')
             data_as_binary = b64encode(b.getvalue())
