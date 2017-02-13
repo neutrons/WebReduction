0dbda255ee562efe9ed5c76c1af9511a5e1d4bfd
Ricardo M. Ferraz Leal <ricleal@gmail.com>
2017-02-13 15:06:17 -0500

    Job detail done

--- src/server/apps/results/urls.py
+++ src/server/apps/results/urls.py
@@ -6,5 +6,6 @@ from .views import *
 urlpatterns = [
      # Configurations
     url(r'^$', JobsList.as_view(), name='list'),
+    url(r'^(?P<pk>\d+)$', JobDetail.as_view(), name='detail'),
     url(r'^live$', TemplateView.as_view(template_name='results/live.html'), name='live'),
 ]
--- src/server/apps/results/views.py
+++ src/server/apps/results/views.py
@@ -1,11 +1,12 @@
 
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django_remote_submission.models import Job
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
-# Create your views here.
+from server.apps.sans.models import ModelMixin
 
-
+# For the job details to share the same function as yhe sans models
+Job.get_all_fields = ModelMixin.get_all_fields
 
 class JobMixin(object):
 
@@ -20,4 +21,13 @@ class JobsList(LoginRequiredMixin, JobMixin, ListView):
 
     '''
     template_name = 'results/job_list.html'
+
+class JobDetail(LoginRequiredMixin, JobMixin, DetailView):
+    '''
+    Detail of a configuration
+    '''
+    template_name = 'results/job_detail.html'
     
+    def get_queryset(self):
+        queryset = super(JobDetail, self).get_queryset()
+        return queryset.filter(id=self.kwargs['pk'])
