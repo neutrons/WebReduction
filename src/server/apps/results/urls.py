207dcbe16c3919f492bd3516d5a9441c50ee8fdc
Ricardo M. Ferraz Leal <ricleal@gmail.com>
2017-02-14 14:58:27 -0500

    Log live fully working

--- src/server/apps/results/urls.py
+++ src/server/apps/results/urls.py
@@ -5,8 +5,8 @@ from .views import *
 
 urlpatterns = [
      # Configurations
-    url(r'^$', JobsList.as_view(), name='list'),
-    url(r'^(?P<pk>\d+)$', JobDetail.as_view(), name='detail'),
-    url(r'^livelog/(?P<pk>\d+)$', JobLiveLog.as_view(), name='livelog'),
-    url(r'^live$', TemplateView.as_view(template_name='results/job_live.html'), name='live'),
+    url(r'^$', JobsList.as_view(), name='job_list'),
+    url(r'^(?P<pk>\d+)$', JobDetail.as_view(), name='job_detail'),
+    url(r'^livelog/(?P<pk>\d+)$', JobLiveLog.as_view(), name='job_log_live'),
+    url(r'^live$', TemplateView.as_view(template_name='results/job_live.html'), name='job_list_live'),
 ]
