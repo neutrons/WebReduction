95462c181ecb71c79c933b8b140b3227b95d8a5f
Ricardo M. Ferraz Leal <ricleal@gmail.com>
2017-06-09 13:20:42 -0400

    Added cache to plot view

--- src/server/apps/results/urls.py
+++ src/server/apps/results/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import url
 from django.views.generic import TemplateView
+from django.views.decorators.cache import cache_page
 
 from .views import *
 
@@ -9,6 +10,6 @@ urlpatterns = [
     url(r'^(?P<pk>\d+)$', JobDetail.as_view(), name='job_detail'),
     url(r'^livelog/(?P<pk>\d+)$', JobLiveLog.as_view(), name='job_log_live'),
     url(r'^live$', TemplateView.as_view(template_name='results/job_live.html'), name='job_list_live'),
-    #
-    url(r'^result/(?P<pk>\d+)/plot$', ResultPlot.as_view(), name='result_plot'),
+    # Cache for 60 * 15 seconds
+    url(r'^result/(?P<pk>\d+)/plot$', cache_page(60 * 15)(ResultPlot.as_view()), name='result_plot'),
 ]
